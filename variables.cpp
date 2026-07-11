6ca1f9850d94422db53a8988e2d40754f0c5fc02
atjon <atjon@ucsd.edu>
2026-07-10 22:22:19 -0700

    adding print and variables examples

---
+++ variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    int x = 5;
+    double a = 6.5;
+
+    std::cout << "x is ";
+    std::cout << x;
+    std::cout << "\n";
+    
+    std::cout << "a is ";
+    std::cout << a;
+    std::cout << "\n";
+ 
+}
