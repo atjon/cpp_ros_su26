ba0fdf50225cd0ca86f2fcaefda8e16945f4a425
atjon <atjon@ucsd.edu>
2026-07-13 19:04:20 -0700

    parameters in functions and scopes

--- functions.cpp
+++ functions.cpp
@@ -22,12 +22,18 @@ int returnSeven() {
     return 7;
 }
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
     std::cout << "Starting main()\n";
     doPrint(); // Interrupts main() and calls doPrint()
     std::cout << "Function calls other functions\n";
     doA();
     std::cout << returnSix() << " " << returnSeven() << "!\n";
+    std::cout << "add(4, 5) = " << add(4,5) << " " 
+    "add(5, 6)" << add(5, 6) << "\n";
     std::cout << "Ending main()\n"; //This is after the doPrint() statement
 
     return 0;
