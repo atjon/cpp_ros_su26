12570d66b08d06c3849409831068f60cdcde4f9f
atjon <tjaidan241@gmail.com>
2026-07-19 22:38:06 -0700

    learninga about constexpr and its unique properies

---
+++ constexpr.cpp
@@ -0,0 +1,27 @@
+#include <climits>
+#include <iostream>
+
+int main() {
+
+
+    const int t = 20; // clearly a constant variable & will NEVER change
+                      // ideally, if compiler could, it should want to optimize
+                      // this piece of code during compile time, instead of it 
+                      // taking valuable runtime 
+
+    constexpr int x = 5; // constexpr answers this need. A constexpr variable 
+                         // must always be defined w/ a constant expression
+                         // useful when combined with template meta programming
+
+    if constexpr (x == 5) { // when the conditional is a constant expression, 
+                            // its best practice to use constexpr if rather than not
+                            // it compeletely gets rid of the entire block of code 
+                            // that won't be executed at runtime 
+        std::cout << "x is normal\n";
+    } else {
+        std::cout << "x isn't normal\n";
+    }
+
+    
+    return 0;
+}
